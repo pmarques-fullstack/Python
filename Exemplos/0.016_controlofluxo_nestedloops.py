064c74d1c2248b7aa984b2a4d7a659c126d8136d
Pedro Marques <27385056+pmarques-fullstack@users.noreply.github.com>
2023-03-14 21:42:55 +0000

    Create 0.016_controlofluxo_nestedloops.py

---
+++ Exemplos/0.016_controlofluxo_nestedloops.py
@@ -0,0 +1,9 @@
+frutas = ["maçã", "banana", "cereja"]
+adj = ["fresca", "grande", "saborosa"]
+
+for x in frutas:
+	for y in adj:
+  		print(x, y)
+      
+for x in [0, 1, 2]:
+	pass # usado na declaração para ficar vazia
