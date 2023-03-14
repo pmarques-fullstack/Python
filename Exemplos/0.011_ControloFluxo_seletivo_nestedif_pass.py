2081cca1fd557806533c430b49d19f95ee62a9f3
Pedro Marques <27385056+pmarques-fullstack@users.noreply.github.com>
2023-03-14 21:24:24 +0000

    Create 0.011_ControloFluxo_seletivo_nestedif_pass.py

---
+++ Exemplos/0.011_ControloFluxo_seletivo_nestedif_pass.py
@@ -0,0 +1,12 @@
+x = 41
+if x > 10:
+	print("Acima de dez,")
+	if x > 20:
+		print("e também acima de 20!")
+	else:
+		print("mas não acima de 20.")
+
+a = 33
+b = 200
+if b > a:
+	pass # utiliza-se quando se quer manter a declaração vazia
