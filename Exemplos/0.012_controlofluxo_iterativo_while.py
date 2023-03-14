0680e0b000842036e27f6a46e16bf8478026b809
Pedro Marques <27385056+pmarques-fullstack@users.noreply.github.com>
2023-03-14 21:28:33 +0000

    Create 0.012_controlofluxo_iterativo_while.py

---
+++ Exemplos/0.012_controlofluxo_iterativo_while.py
@@ -0,0 +1,11 @@
+conta = 5
+while ( conta >= 0 ):
+	print("Lançamento em breve…",conta)
+	conta = conta - 1
+  
+lista = (“estudante1”,”estudante2”,”estudante3”)
+for estudante in lista:
+	print(estudante)
+  
+for x in "banana":
+  print(x)
