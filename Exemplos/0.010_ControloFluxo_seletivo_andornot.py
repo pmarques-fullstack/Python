1417352274b40afc5c4e4cfe94c9d99e3f5e96db
Pedro Marques <27385056+pmarques-fullstack@users.noreply.github.com>
2023-03-14 21:21:32 +0000

    Create 0.010_ControloFluxo_seletivo_andornot.py

---
+++ Exemplos/0.010_ControloFluxo_seletivo_andornot.py
@@ -0,0 +1,9 @@
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+	print("As duas condições são verdadeiras")
+if a > b or a > c:
+	print("Pelo menos uma das condições é verdadeira")
+if not b > c:
+	print("b NÃO é maior que c")
